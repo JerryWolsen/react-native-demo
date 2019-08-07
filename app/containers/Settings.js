3f28c02138e9d69d9ee10468e53426bd601ce186
Jerry.WuTJ <Jerry.WuTJ@homecredit.cn>
2019-08-07 10:45:54 +0800

    fix android gradle error

--- app/containers/Settings.js
+++ app/containers/Settings.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
-import { StyleSheet, SafeAreaView} from "react-native"
+import { StyleSheet, SafeAreaView, Platform, Text} from "react-native"
 import MapView from '../components/MapView'
 
 export default class SettingsScreen extends Component {
   render() {
     return (
       <SafeAreaView style={styles.setting}>
+        {Platform.OS === 'ios' ?
         <MapView width={'100%'}
                  height={'100%'}
                  backgroundColor={'white'}
                  posLatitude={39.152079}
                  posLongitude={117.178526}
                  posAddress={'陆家嘴金融广场'}
-                 posName={'第三方调查'}/>
+                 posName={'第三方调查'}/> : <Text >Settings</Text>}
       </SafeAreaView>
     )
   }
@@ -22,7 +23,7 @@ const styles = StyleSheet.create({
   setting: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
     backgroundColor: '#F5FCFF',
   }
 })
